51729e82ce37a8373ac0c65978e9beb401487b80
mokwathedeveloper <moffatmokwa12@gmail.com>
2026-02-28 13:56:42 +0300

    fix: correct import order for use client directive

--- components/admin/edit-testimonial-form.tsx
+++ components/admin/edit-testimonial-form.tsx
@@ -1,5 +1,5 @@
-import { logger } from "@/lib/logger";
 "use client";
+import { logger } from "@/lib/logger";
 
 import { useState, useTransition } from "react";
 import { useRouter } from "next/navigation";
